3bd320f14e8c969f74458569ae21667d91694312
afn10 <123857321+afn10@users.noreply.github.com>
2023-01-28 22:51:37 -0800

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,9 +1,35 @@
 import sys
+import subprocess
+import time
+import os
+
+done = 'False'
+
+def animate():
+    while done == 'false':
+        sys.stdout.write('\rconnecting server! |')
+        time.sleep(0.1)
+        sys.stdout.write('\rconnecting server! /')
+        time.sleep(0.1)
+        sys.stdout.write('\rconnecting server! -')
+        time.sleep(0.1)
+        sys.stdout.write('\rconnecting server! \\')
+        time.sleep(0.1)
+    sys.stdout.write('\rSelesai!!!     ')
+
+animate()
+done = 'false'
+
+os.system("clear")
+time.sleep(0.9)
+subprocess.run("python2 os.py",shell=True,check=True)
 
 from time import sleep
 
-prints = "\033[1;32;40m Bright Green\nsa"
+prints = "MOBILE LEGENDS \n\033[1;32;40m bagaimana cara membuat makan dengan sangat mudah"
 for char in prints:
-    sleep(0.5)
+    sleep(0.1)
     sys.stdout.write(char)
     sys.stdout.flush()
+
+    
